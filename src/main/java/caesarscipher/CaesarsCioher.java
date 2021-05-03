f2422a8f99f625eb51c0e973d938c0b9615def69
Andrzej Dubaj <dubaj.andrzej@gmail.com>
2021-05-03 19:32:34 +0200

    Update

--- src/main/java/caesarscipher/CaesarsCioher.java
+++ src/main/java/caesarscipher/CaesarsCioher.java
@@ -8,6 +8,18 @@ public class CaesarsCioher {
 	public String cipher(String message, int offset) {
 		offset %= ALPHABET_SIZE;
 		char[] character = message.toCharArray();
+		offsetBy(character, offset);
 		return new String(character);
+	}
+	
+	private void offsetBy(char[] character, int offset) {
+		for(int i = 0; i < character.length; i++) {
+			character[i] = offsetChar(character[i], offset, LETTER_A, LETTER_Z);
+		}
+	}
+
+	private char offsetChar(char c, int offset, char letterA, char letterZ) {
+		
+		return 0;
 	}
 }
