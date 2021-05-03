af82cc7d06b92b03f6e9e7cb9fb0d4496933b3a6
Andrzej Dubaj <dubaj.andrzej@gmail.com>
2021-05-03 20:25:33 +0200

    Cleanup

--- src/main/java/caesarscipher/Main.java
+++ src/main/java/caesarscipher/Main.java
@@ -3,15 +3,16 @@ package caesarscipher;
 public class Main {
 
 	public static void main(String[] args) {
+		
 		String message = "Welcome to this Ceasars application";
 		int offset = 12; 
 		
 		CaesarsCipher caesarsCipher = new CaesarsCipher(); 
-		caesarsCipher.cipher(message, offset);
+		String cipherMessage = caesarsCipher.cipher(message, offset);
 		
 		System.out.println("Message: " + message);
 		System.out.println("Offset: " + offset);
-		System.out.println("Cipher message: " + caesarsCipher.cipher(message, offset));
+		System.out.println("Cipher message: " + cipherMessage);
 
 	}
 }
